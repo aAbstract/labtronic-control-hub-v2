08b19e0d0a469dae4a9493e07fd226ac0cce495b
aAbstract <elshareslam@gmail.com>
2025-01-23 13:37:30 +0200

    Added Micropython OBD_BLE_Interceptor Firmware

--- e2e/_vspi/test_vspis.py
+++ e2e/_vspi/test_vspis.py
@@ -11,6 +11,7 @@ from e2e._vspi.test_drivers import (
 
     ltd_driver_lt_ht004,
     ltd_driver_lt_ht103,
+    ltd_driver_lt_ht107,
     ltd_driver_lt_ht113,
 
     ltd_driver_lt_to101,
@@ -26,6 +27,7 @@ from e2e._vspi.test_drivers import (
 lt_ch000_vspi = VSPI(device_model='LT-CH000', device_driver=ltd_driver_lt_ch000)
 lt_ht004_vspi = VSPI(device_model='LT-HT004', device_driver=ltd_driver_lt_ht004)
 lt_ht103_vspi = VSPI(device_model='LT-HT103', device_driver=ltd_driver_lt_ht103, control_feedback_map={12: 5, 13: 6})
+lt_ht107_vspi = VSPI(device_model='LT-HT107', device_driver=ltd_driver_lt_ht107)
 lt_ht113_vspi = VSPI(device_model='LT-HT113', device_driver=ltd_driver_lt_ht113)
 
 lt_to101_vspi = VSPI(device_model='LT-TO101', device_driver=ltd_driver_lt_to101)
